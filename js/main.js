85526614c779f96c6e29b1656365ad3f90365c8c
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-01-08 22:45:40 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+let elBody = document.querySelector("body");
+let elBtn = document.querySelector(".dark");
+let elForm = document.querySelector(".form");
+let elInput = document.querySelector(".form-control");
+let elSelect = document.querySelector(".form-select");
+let elList = document.querySelector(".list");
+let listTemplate = document.querySelector(".listTemplate").content;
+
+// Theme
+elBtn.addEventListener("click", function () {
+  elBody.classList.toggle("mode");
+});
+let fragment = document.createDocumentFragment();
+
+const renderCountry = (array, node) => {
+  node.innerHTML = [];
+  array.forEach((el) => {
+    let clonedTemplate = listTemplate.cloneNode(true);
+    clonedTemplate.querySelector(".card-img-top").src = el.flags.svg;
+    clonedTemplate.querySelector(".card-title").textContent = el.name.common;
+    clonedTemplate.querySelector(
+      ".card-text-population"
+    ).innerHTML = `<i class="fa-solid fa-arrow-up-right-dots"></i> Population: <strong>${el.population}</strong>`;
+
+    clonedTemplate.querySelector(
+      ".card-text-region"
+    ).innerHTML = `<i class="fa-solid fa-earth-oceania"></i> Region: <strong>${el.region}</strong>`;
+
+    clonedTemplate.querySelector(
+      ".card-text-capital"
+    ).innerHTML = `<i class="fa-solid fa-building-columns"></i> Capital: <strong>${el.capital}</strong>`;
+
+    fragment.appendChild(clonedTemplate);
+  });
+  node.appendChild(fragment);
+};
+
+async function getCountries() {
+  // Fetch all the features, and clone will use this
+  let response = await fetch("https://restcountries.com/v3.1/all");
+  let data = await response.json();
+  renderCountry(data, elList);
+}
+getCountries();
+
+//* Compare Input name
+async function compareInput() {
+  let response = await fetch(
+    `https://restcountries.com/v3.1/name/${elInput.value}`
+  );
+  let data = await response.json();
+  renderCountry(data, elList);
+}
+
+async function compareInputRegion() {
+  let response = await fetch(
+    `https://restcountries.com/v3.1/region/${elInput.value}`
+  );
+  let data = await response.json();
+  renderCountry(data, elList);
+}
+
+async function compareInputCapital() {
+  let response = await fetch(
+    `https://restcountries.com/v3.1/capital/${elInput.value}`
+  );
+  let data = await response.json();
+  renderCountry(data, elList);
+}
+
+async function compareInputPopulationNum() {
+  let response = await fetch(
+    `https://restcountries.com/v3.1/all/population/${elInput.value}`
+  );
+  let data = await response.json();
+  renderCountry(data, elList);
+}
+
+elForm.addEventListener("input", (evt) => {
+  evt.preventDefault();
+
+  if (elInput.value !== "") {
+    compareInput();
+    //     compareInputRegion();
+    //     compareInputCapital();
+    //     compareInputPopulationNum();
+  }
+
+  // Executes to the DOM
+});
+elForm.addEventListener("submit", (evt) => evt.preventDefault());
